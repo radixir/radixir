adb649b14176b5f3085148413b59680a5af08edb
Jon-Eric Cook <joneric.cook@blockfi.com>
2021-11-24 11:09:02 -0800

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -6,23 +6,39 @@ defmodule Radixir.MixProject do
       app: :radixir,
       version: "0.0.1",
       elixir: "~> 1.13.0-rc.1",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Radixir",
+      source_url: "https://github.com/radixir/radixir"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp description() do
+    """
+    An Elixir wrapper for Radix APIs and functionality.
+    """
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      maintainers: ["Jon-Eric Cook"],
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/radixir/radixir"}
     ]
   end
 end
