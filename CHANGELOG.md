6dd35ccf848c28b655f31e22c9b8b02948ec0671
Jon-Eric Cook <joneric.cook@blockfi.com>
2021-11-24 11:27:44 -0800

    update mix.exs

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.0.1 (11.24.2021)
+
+- Create on hex.pm
--- mix.exs
+++ mix.exs
@@ -1,18 +1,20 @@
 defmodule Radixir.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/radixir/radixir"
+  @version "0.0.1"
+
   def project do
     [
       app: :radixir,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.13.0-rc.1",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
       deps: deps(),
-      name: "Radixir",
-      source_url: "https://github.com/radixir/radixir"
+      source_url: @source_url
     ]
   end
 
@@ -37,10 +39,9 @@ defmodule Radixir.MixProject do
   defp package() do
     [
       maintainers: ["Jon-Eric Cook"],
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE CHANGELOG.md),
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/radixir/radixir"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
