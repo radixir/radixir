3507607a3e0800203e88c5928b41fcad76ad4a4b
Jon-Eric Cook <joneric.cook@blockfi.com>
2021-12-01 14:50:29 -0800

    save keypairs in .json file

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,4 @@
 import Config
 
 config :radixir,
-  radix_node_url: System.fetch_env!("RADIX_NODE_URL"),
-  private_keys: System.fetch_env!("PRIVATE_KEYS"),
-  radix_testnet: System.fetch_env!("RADIX_TESTNET")
+  radix_node_url: System.fetch_env!("RADIX_NODE_URL")
--- lib/radixir/config.ex
+++ lib/radixir/config.ex
@@ -2,12 +2,4 @@ defmodule Radixir.Config do
   def radix_node_url do
     Application.fetch_env!(:radixir, :radix_node_url)
   end
-
-  def private_keys do
-    Application.fetch_env!(:radixir, :private_keys)
-  end
-
-  def radix_testnet do
-    Application.fetch_env!(:radixir, :radix_testnet)
-  end
 end
