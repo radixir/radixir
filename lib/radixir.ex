b466cd8ff1f20adb6ec969398d713108f2d8df68
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-01-21 09:44:37 -0800

    add send_xrd functionality

--- lib/radixir.ex
+++ lib/radixir.ex
@@ -1,26 +1,41 @@
 defmodule Radixir do
-  # alias Radixir.GatewayAPI
-  # alias Radixir.Keypair
-  # alias Radixir.Utils
+  alias Radixir.GatewayAPI
+  alias Radixir.Keypair
+  alias Radixir.Utils
 
-  # def send_xrd(from, to, amount, private_key) do
-  #   with {:ok, %{private_key: private_key, public_key: public_key_of_signer}} <-
-  #          Keypair.from_private_key(private_key),
-  #        {:ok, %{"rri" => rri}} <- GatewayAPI.get_native_token(),
-  #        actions <- [
-  #          %{
-  #            amount: amount,
-  #            from: from,
-  #            rri: rri,
-  #            to: to,
-  #            type: "TokenTransfer"
-  #          }
-  #        ],
-  #        {:ok, %{"transaction" => %{"blob" => blob, "hashOfBlobToSign" => hash_of_blob_to_sign}}} <-
-  #          GatewayAPI.build_transaction(actions, from),
-  #        :ok <- Utils.verify_blob_hash(blob, hash_of_blob_to_sign) do
-  #     signature_der = Keypair.sign_data(hash_of_blob_to_sign, private_key)
-  #     GatewayAPI.finalize_transaction(blob, signature_der, public_key_of_signer, true)
-  #   end
-  # end
+  def send_xrd(from, to, amount, private_key) do
+    with {:ok, %{private_key: private_key, public_key: public_key}} <-
+           Keypair.from_private_key(private_key),
+         {:ok, %{"token" => %{"token_identifier" => %{"rri" => rri}}}} <-
+           GatewayAPI.get_native_token_info(),
+         actions <- [
+           %{
+             type: "TransferTokens",
+             from_account: %{
+               address: from
+             },
+             to_account: %{
+               address: to
+             },
+             amount: %{
+               token_identifier: %{
+                 rri: rri
+               },
+               value: amount
+             }
+           }
+         ],
+         {:ok,
+          %{
+            "transaction_build" => %{
+              "payload_to_sign" => payload_to_sign,
+              "unsigned_transaction" => unsigned_transaction
+            }
+          }} <-
+           GatewayAPI.build_transaction(actions, from),
+         :ok <- Utils.verify_blob_hash(unsigned_transaction, payload_to_sign) do
+      signature_bytes = Keypair.sign_data(payload_to_sign, private_key)
+      GatewayAPI.finalize_transaction(unsigned_transaction, signature_bytes, public_key, true)
+    end
+  end
 end
--- lib/radixir/gateway_api.ex
+++ lib/radixir/gateway_api.ex
@@ -6,13 +6,13 @@ defmodule Radixir.GatewayAPI do
     HTTP.post(Config.radix_gateway_api_url(), "/gateway", %{})
   end
 
-  def derive_account_identifier(hex_public_key) do
+  def derive_account_identifier(public_key_hex) do
     HTTP.post(Config.radix_gateway_api_url(), "/account/derive", %{
       network_identifier: %{
         network: Config.network()
       },
       public_key: %{
-        hex: hex_public_key
+        hex: public_key_hex
       }
     })
   end
@@ -60,6 +60,114 @@ defmodule Radixir.GatewayAPI do
       },
       cursor: cursor,
       limit: limit
+    })
+  end
+
+  def get_native_token_info() do
+    HTTP.post(Config.radix_gateway_api_url(), "/token/native", %{
+      network_identifier: %{
+        network: Config.network()
+      }
+    })
+  end
+
+  def get_token_info(rri) do
+    HTTP.post(Config.radix_gateway_api_url(), "/token", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      token_identifier: %{
+        rri: rri
+      }
+    })
+  end
+
+  def derive_token_identifier(public_key_hex, symbol) do
+    HTTP.post(Config.radix_gateway_api_url(), "/token/derive", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      public_key: %{
+        hex: public_key_hex
+      },
+      symbol: symbol
+    })
+  end
+
+  def get_validator(address) do
+    HTTP.post(Config.radix_gateway_api_url(), "/validator", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      validator_identifier: %{
+        address: address
+      }
+    })
+  end
+
+  def derive_validator_identifier(public_key_hex) do
+    HTTP.post(Config.radix_gateway_api_url(), "/validator/derive", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      public_key: %{
+        hex: public_key_hex
+      }
+    })
+  end
+
+  def get_validators() do
+    HTTP.post(Config.radix_gateway_api_url(), "/validators", %{
+      network_identifier: %{
+        network: Config.network()
+      }
+    })
+  end
+
+  def get_transaction_rules() do
+    HTTP.post(Config.radix_gateway_api_url(), "/transaction/rules", %{
+      network_identifier: %{
+        network: Config.network()
+      }
+    })
+  end
+
+  def build_transaction(actions, fee_payer_address) do
+    HTTP.post(Config.radix_gateway_api_url(), "/transaction/build", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      actions: actions,
+      fee_payer: %{address: fee_payer_address}
+    })
+  end
+
+  def finalize_transaction(unsigned_transaction, signature_bytes, public_key_hex, submit) do
+    HTTP.post(Config.radix_gateway_api_url(), "/transaction/finalize", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      unsigned_transaction: unsigned_transaction,
+      signature: %{bytes: signature_bytes, public_key: %{hex: public_key_hex}},
+      submit: submit
+    })
+  end
+
+  def submit_transaction(signed_transaction) do
+    HTTP.post(Config.radix_gateway_api_url(), "/transaction/submit", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      signed_transaction: signed_transaction
+    })
+  end
+
+  def get_transaction_status(transaction_identifier_hash) do
+    HTTP.post(Config.radix_gateway_api_url(), "/transaction/status", %{
+      network_identifier: %{
+        network: Config.network()
+      },
+      transaction_identifier: %{hash: transaction_identifier_hash}
     })
   end
 end
