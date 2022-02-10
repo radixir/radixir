9107624b52d0668778c4a93e649d5522601b79a5
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-02-10 12:10:53 -0800

    add todo note

--- lib/radixir/core/operation_group.ex
+++ lib/radixir/core/operation_group.ex
@@ -2,6 +2,7 @@ defmodule Radixir.Core.OperationGroup do
   def create(operations, metadata) do
     %{
       operations: operations,
+      # TODO: figure out what goes here
       metadata: metadata
     }
   end
