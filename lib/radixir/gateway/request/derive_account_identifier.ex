7b4ef256f1b1c4056b62d4d7061e6937605afdcb
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-02-13 13:57:53 -0800

    remove unused alias

--- lib/radixir/gateway/request/derive_account_identifier.ex
+++ lib/radixir/gateway/request/derive_account_identifier.ex
@@ -1,7 +1,5 @@
 defmodule Radixir.Gateway.Request.DeriveAccountIdentifier do
   alias Radixir.StitchPlan
-  alias Radixir.Schema.Gateway
-  alias Radixir.Util
 
   defdelegate network_identifier(stitch_plans, params \\ []), to: StitchPlan
   defdelegate public_key(stitch_plans, params), to: StitchPlan
