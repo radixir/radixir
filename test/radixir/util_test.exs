c49a809c93f3749f5f8b976d4f601557afe4da2c
Jon-Eric Cook <joneric.cook@blockfi.com>
2022-03-13 20:34:03 -0700

    add decrypt_message tests

--- test/radixir/util_test.exs
+++ test/radixir/util_test.exs
@@ -120,5 +120,95 @@ defmodule Radixir.UtilTest do
       assert {:ok, message} ==
                Util.decrypt_message(encrypted_message, private_key_2, account_address_1)
     end
+
+    test "catches private_key that contains non hexadecimal digitis" do
+      private_key = "79271eb15d58259ca1d58e8c49db2c208fa4b17e6e48c2ab2c3610855e4d747g"
+      account_address = "rdx1qsp8aa6y6cgvuxqxgn2cm5q7jn7ng6ppdhye9npwpkcfvxtnlf4l2csqf5dvh"
+      message = "hello there"
+
+      assert {:error, "private_key must only have hexadecimal digits"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches private_key that is too long" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc344"
+      account_address = "rdx1qsp8aa6y6cgvuxqxgn2cm5q7jn7ng6ppdhye9npwpkcfvxtnlf4l2csqf5dvh"
+      message = "hello there"
+
+      assert {:error, "private_key must be 64 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has non alpha num character in it" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "rdx1qsp8aa6y6cgvuxqxgn2cm5q7jn7ng6ppdhye9npwpkcfvxtnlf4l2csqf5dv."
+      message = "hello there"
+
+      assert {:error, "address must only have alpha numeric characters"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong prefix" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "fdx1qsp8aa6y6cgvuxqxgn2cm5q7jn7ng6ppdhye9npwpkcfvxtnlf4l2csqf5dv"
+      message = "hello there"
+
+      assert {:error, "address must start with rdx, tdx, rv, tv, rn or tn"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong length - rdx" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "rdx1qsp8aa6y6cgvuxqxgn2cm5q7jn7ng6ppdhye9npwpkcfvxtnlf4l2csqf5dvhh"
+      message = "hello there"
+
+      assert {:error, "account address must be 65 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong length - tdx" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "tdx1qspycakrstk7h5kxj5e5e2areycqnf5l39wzfhthcyshud9lh6mktvcvc83pkk"
+      message = "hello there"
+
+      assert {:error, "account address must be 65 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong length - rv" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "rv1qfx8dsuzah4a9354xdx2hg7fxqy6d8uftsjd6a7py9lrf0a7kajmxz0rdrtt"
+      message = "hello there"
+
+      assert {:error, "validator address must be 62 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong length - tv" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "tv1qfx8dsuzah4a9354xdx2hg7fxqy6d8uftsjd6a7py9lrf0a7kajmxykytvuu"
+      message = "hello there"
+
+      assert {:error, "validator address must be 62 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong length - rn" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "rn1qfx8dsuzah4a9354xdx2hg7fxqy6d8uftsjd6a7py9lrf0a7kajmx6ufa044"
+      message = "hello there"
+
+      assert {:error, "node address must be 62 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
+
+    test "catches address that has wrong length - tn" do
+      private_key = "519b6c5c1db1fab3e8513ad18b419082c81844ef66b24f850f9ab7366f7efc34"
+      account_address = "tn1qfx8dsuzah4a9354xdx2hg7fxqy6d8uftsjd6a7py9lrf0a7kajmxu9wmqzz"
+      message = "hello there"
+
+      assert {:error, "node address must be 62 characters long"} =
+               Util.decrypt_message(message, private_key, account_address)
+    end
   end
 end
